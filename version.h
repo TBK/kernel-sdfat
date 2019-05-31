ab4594b6a37e102f4e2f4591ecad1b5cd3a93274
TBK <tbk@jjtc.eu>
2019-05-31 15:18:44 +0200

    Bump version

--- version.h
+++ version.h
@@ -22,4 +22,4 @@
 /*  PURPOSE : sdFAT File Manager                                        */
 /*                                                                      */
 /************************************************************************/
-#define SDFAT_VERSION	"2.1.8.1"
+#define SDFAT_VERSION	"2.1.8.2"
